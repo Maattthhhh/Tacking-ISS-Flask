c2d3608fd696ca42f6f874bcdb6a3b47975a96f9
maattthhhh <montilla_mark_mathew@yahoo.com>
2024-02-17 08:36:56 +0800

    Tacking ISS in Flask

---
+++ app.py
@@ -0,0 +1,138 @@
+from flask import Flask, render_template
+import re
+import bs4
+import requests
+import json
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+from geopy.geocoders import Nominatim
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    #sends request to get the latitude and longitude of the International Space Station
+    req = requests.get('https://api.wheretheiss.at/v1/satellites/25544', timeout=3);
+    data = req.json();
+    latitude = float(data['latitude']);
+    longitude = float(data['longitude']);
+
+    print("ISS Latitude: ", latitude);
+    print("ISS Longitude: ", longitude);
+
+    #uses an API to locate which city, country and/or continent the coordinates point towards
+    req = requests.get("https://api.bigdatacloud.net/data/reverse-geocode-client?latitude="+str(latitude)+"&longitude="+str(longitude)+"&localityLanguage=en");
+    if req.status_code == 200:
+        iss = req.json();
+
+        def get_condition(city):
+            url = "http://wttr.in/" + city + "?format=%t"
+            page = uReq(url)
+            raw = page.read()
+            condition = raw.decode("utf-8")
+            return condition
+
+        if "GMT" in iss['locality'] or iss['locality'] =='':
+            if "GMT" in iss['countryName'] or iss['countryName'] =='':
+                if "GMT" in iss['continent'] or iss['continent'] =='':
+                    my_url = "https://en.wikipedia.org/wiki/"+iss['localityInfo']['informative'][0]['name'];
+                    response = requests.get(my_url)
+                    if response.status_code == 200:
+                        page_soup = soup(response.text, "html.parser")
+                        paragraphs = page_soup.findAll('p')
+                        filter_phrases = ["was the", "is the", "are the", "was a", "is a", "are a"]
+
+                        for p in paragraphs:
+                            paragraph_text = p.get_text().lower()
+                            if any(phrase in paragraph_text for phrase in filter_phrases):
+                                text_with_notations = p.get_text()
+                                cleaned_text = re.sub(r'\[\d+\]', '', text_with_notations)
+                                print()
+                                print("The International Space Station is currently near "+iss['localityInfo']['informative'][0]['name']+".");
+                                print()
+                                print(cleaned_text)
+                                break
+                else:
+                    my_url = "https://en.wikipedia.org/wiki/"+iss['continent'];
+                    response = requests.get(my_url)
+
+                    #uses webscraping to get general info on the current location through Wikipedia
+                    if response.status_code == 200:
+                        page_soup = soup(response.text, "html.parser")
+                        paragraphs = page_soup.findAll('p')
+                        filter_phrases = ["was the", "is the", "are the", "was a", "is a", "are a"]
+
+                        for p in paragraphs:
+                            paragraph_text = p.get_text().lower()
+                            if any(phrase in paragraph_text for phrase in filter_phrases):
+                                text_with_notations = p.get_text()
+                                cleaned_text = re.sub(r'\[\d+\]', '', text_with_notations)
+                                print()
+                                print("The International Space Station is currently near "+iss['continent']);
+                                print()
+                                print(cleaned_text)
+                                break
+                    else:
+                        print()
+                        cleaned_text = "The International Space Station is currently near "+iss['continent']
+            else:
+                my_url = "https://en.wikipedia.org/wiki/"+iss['countryName'];
+                response = requests.get(my_url)
+
+                if response.status_code == 200:
+                    page_soup = soup(response.text, "html.parser")
+                    paragraphs = page_soup.findAll('p')
+                    filter_phrases = ["was the", "is the", "are the", "was a", "is a", "are a"]
+
+                    for p in paragraphs:
+                        paragraph_text = p.get_text().lower()
+                        if any(phrase in paragraph_text for phrase in filter_phrases):
+                            text_with_notations = p.get_text()
+                            cleaned_text = re.sub(r'\[\d+\]', '', text_with_notations)
+                            print()
+                            print("The International Space Station is currently near "+iss['countryName']);
+                            print()
+                            print(cleaned_text)
+                            break
+                else:
+                    print()
+                    cleaned_text = "The International Space Station is currently near "+iss['countryName']
+        else:
+            my_url = "https://en.wikipedia.org/wiki/"+iss['locality'];
+            response = requests.get(my_url)
+
+            if response.status_code == 200:
+                page_soup = soup(response.text, "html.parser")
+                paragraphs = page_soup.findAll('p')
+                filter_phrases = ["was the", "is the", "are the", "was a", "is a", "are a"]
+
+                for p in paragraphs:
+                    paragraph_text = p.get_text().lower()
+                    if any(phrase in paragraph_text for phrase in filter_phrases):
+                        text_with_notations = p.get_text()
+                        cleaned_text = re.sub(r'\[\d+\]', '', text_with_notations)
+                        print()
+                        print("The International Space Station is currently near "+iss['locality']+", "+iss['countryName']+".");
+                        print()
+                        print(cleaned_text)
+                        break
+
+                city = iss['locality']
+                condition = get_condition(city)
+                print("The current weather in "+iss['locality']+" is "+condition)
+                
+            else:
+                print()
+                print("The International Space Station is currently near "+iss['locality']+", "+iss['countryName']+".");
+                city = iss['locality']
+                condition = get_condition(city)
+                cleaned_text = "The current weather in " + city + " is " + condition
+
+    return render_template('index.html', cleaned_text=cleaned_text)
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
